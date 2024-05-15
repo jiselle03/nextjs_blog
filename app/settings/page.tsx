7588bf93a76bbc58c20fc58aa3b954da61ba55f4
Jiselle Liu <jiselle03@gmail.com>
2024-05-15 21:48:09 +0800

    Add settings page and navbar component

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -2,7 +2,6 @@
 
 import { useState } from 'react';
 import { IoSearch } from "react-icons/io5";
-import BlogPost from '@/components/blog-post';
 import NavBar from '@/components/nav-bar';
 
 const Dashboard = () => {
@@ -12,17 +11,10 @@ const Dashboard = () => {
     <div className="flex flex-row min-h-screen justify-between p-24 divide-x divide-gray-300">
       {/* Navbar */}
       <NavBar />
-      
 
       {/* Main Content */}
       <div className="flex-grow p-4">
-        {[...Array(4)].map((_, i) => (
-          <BlogPost
-            key={i}
-            username={`username ${i + 1}`}
-            num={i + 1}
-          />
-        ))}
+        
       </div>
 
       {/* Right panel */}
